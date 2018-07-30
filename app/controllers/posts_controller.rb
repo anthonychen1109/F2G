38a3fa9cb3665476bef03e7b157f4d0a5bdfc464
storres8 <stevetorres7562@gmail.com>
2018-07-30 15:18:03 -0400

    added stuff

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to user_path(@post)
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def post_params
+    params.require(:post).permit(:image, :location, :user_id)
+  end
+end
