0368a5bff09d40e919d46c642bf11533b8781d75
github username <github email address>
2018-07-30 17:34:57 -0400

    fixed sessions on signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,5 +15,5 @@ class ApplicationController < ActionController::Base
  def logged_in?
    !!current_user
  end
- 
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+
     if @user.save
-      redirect_to @user
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
       render :new
     end
