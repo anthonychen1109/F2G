53015c1ab49b06e01eb0eca588f4d7366780a31e
github username <github email address>
2018-08-01 14:25:23 -0400

    added likes

--- db/migrate/20180730181446_create_posts.rb
+++ db/migrate/20180730181446_create_posts.rb
@@ -1,10 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :caption 
+      t.string :caption
       t.string :location
       t.integer :user_id
-
+      t.integer :likes, :default => 0
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2018_08_01_135102) do
     t.string "caption"
     t.string "location"
     t.integer "user_id"
+    t.integer "likes", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
