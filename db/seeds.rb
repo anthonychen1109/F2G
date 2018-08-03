8ee4eb866006b720a965681e8e913efdfef7da8e
github username <github email address>
2018-08-03 14:59:41 -0400

    added styling to posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,21 @@
 # User.create(name: "Anthony", bio: "HMMMM", pro_image: "https://i.pinimg.com/originals/3a/4d/fc/3a4dfc434553adf8b111ab3f8aad4d49.jpg")
 
 require 'open-uri'
-30.times do
-  user = User.create(name: Faker::Name.name, password: "password", password_confirmation: "password", bio: Faker::ChuckNorris.fact)
-  # user.avatar.attach(io: File.open('../app/assets/images/posts/'), filename: 'download.jpeg')
-end
+# 30.times do
+#   user = User.create(name: Faker::Name.name, password: "password", password_confirmation: "password", bio: Faker::ChuckNorris.fact)
+#   # user.avatar.attach(io: File.open('../app/assets/images/posts/'), filename: 'download.jpeg')
+# end
 
-10.times do
-  num = rand(1..30)
-
-  # pic = File.open(Dir['app/assets/images/*.jpeg'].sample)
-  post = Post.create(caption: Faker::Seinfeld.quote, location: Faker::Nation.capital_city, user_id: num, post_image: "http://www.personal.psu.edu/users//w/z/wzz5072/mini.jpg")
-  # post.post_image.attach(io: File.open('../app/assets/images/posts/'), filename: 'download.jpeg')
-
-end
+# 10.times do
+#   num = rand(1..30)
+#   # pic = File.open(Dir['app/assets/images/*.jpeg'].sample)
+#   post = Post.create(caption: Faker::Seinfeld.quote, location: Faker::Nation.capital_city, user_id: num, post_image: "http://www.personal.psu.edu/users//w/z/wzz5072/mini.jpg")
+#   # post.post_image.attach(io: File.open('../app/assets/images/posts/'), filename: 'download.jpeg')
+#
+# end
 
 50.times do
-  user = rand(1..30)
-  post = rand(1..300)
+  user = rand(1..4)
+  post = rand(1..15)
   Comment.create(user_id: user, post_id: post, content: Faker::FamilyGuy.quote)
 end
